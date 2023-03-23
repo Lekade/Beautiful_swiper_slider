a63c9ace958e8aa8094d39be27277eee203ea1df
Chkip <kasperovich.deonis@gmail.com>
2023-03-23 22:57:18 +0300

    popup + cursor

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
+    MicroModal.init({
+        openTrigger: 'data-micromodal-open',
+        closeTrigger:'data-micromodal-close',
+        disableFocus:true,
+        disableScroll:true,
+        awaitOpenAnimation:true,
+        awaitCloseAnimation:true
+    });
+
     const swiperIMG = new Swiper('.slider-img', {
         loop: false,
         speed: 2400,
@@ -77,4 +86,49 @@ document.addEventListener('DOMContentLoaded', ()=>{
             delay: .3
         })
     })
+    //cursor
+
+    const body = document.querySelector('body'),
+          cursor = document.querySelector('.cursor'),
+          links = document.getElementsByTagName('a')
+
+    let mouseX = 0, mouseY = 0, posX = 0, posY = 0
+
+    const mouseCoords = e => {
+        mouseX = e.pageX
+        mouseY = e.pageY
+    }
+
+    gsap.to({}, .01, {
+        repeat: -1,
+        onRepeat: () => {
+            posX += (mouseX -posX) / 6
+            posY += (mouseY -posY) / 6
+            gsap.set(cursor, {
+                css: {
+                    left: posX,
+                    top: posY
+                }
+            })
+        }
+    })
+
+    for(let i = 0; i < links.length; i++){
+        links[i].addEventListener('mouseover', () =>{
+            cursor.classList.add('active')
+        })
+        links[i].addEventListener('mouseout', () =>{
+            cursor.classList.remove('active')
+        })
+    }
+
+    body.addEventListener('mousemove', e => {
+        mouseCoords(e)
+        cursor.classList.remove('hidden')
+    })
+
+    body.addEventListener('mouseout', e => {
+        cursor.classList.add('hidden')
+    })
+
 })
